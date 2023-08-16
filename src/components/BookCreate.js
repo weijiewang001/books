2cec825a588e337b6d120c1739d5eec2fb20af54
wayne <weijiewang001@gmail.com>
2023-08-17 01:18:52 +1000

    Section 6: How to handle forms

---
+++ src/components/BookCreate.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function BookCreate({ onCreate }) {
+  const [title, setTitle] = useState('');
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  }
+
+  const handleSumit = (event) => {
+    event.preventDefault()
+    onCreate(title);
+    setTitle('')
+  }
+  return (
+    <div className="book-create">
+      <h3>Add a Book</h3>
+      <form onSubmit={handleSumit}>
+        <label>Title</label>
+        <input value={title} onChange={handleChange} className="input" />
+        <button className="button">Create!</button>
+      </form>
+    </div>)
+}
+
+export default BookCreate;
