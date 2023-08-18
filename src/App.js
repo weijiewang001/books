5caa7a1a25e5f4f325be435eed2c98ae21365262
wayne <weijiewang001@gmail.com>
2023-08-18 11:33:07 +1000

    Section 7: Data Persistence with API Requests

--- src/App.js
+++ src/App.js
@@ -1,34 +1,52 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
+import axios from "axios";
 
 function App() {
   const [books, setBooks] = useState([]);
 
-  const createBook = (title) => {
+  const fetchBooks = async () => {
+    const response = await axios.get('http://localhost:3001/books')
+
+    setBooks(response.data);
+  };
+
+  const createBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title
+    });
+
     const updatedBooks = [
       ...books,
-      {
-        id: Math.round(Math.random() * 9999),
-        title
-      }
+      response.data
     ];
+
     setBooks(updatedBooks)
   };
 
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
+
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     })
     setBooks(updatedBooks);
   };
 
-  const editBookById = (id, newTitle) => {
+  useEffect(()=>{
+    fetchBooks()
+  },[])
+
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`,{
+      title: newTitle
+    });
+
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        return { ...book, ...response.data };
       }
-
       return book;
     })
     setBooks(updatedBooks)
