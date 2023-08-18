5d9246db9e8b8241df1c8240578f7d8e9e135f33
wayne <weijiewang001@gmail.com>
2023-08-18 15:03:18 +1000

    Section 8: Communication Using the Context System

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,8 +1,12 @@
 import BookShow from "./BookShow";
+import useBooksContext from "../hooks/use-books-context";
 
-function BookList({ books, onDelete, onEdit }) {
+
+function BookList() {
+  const { books } = useBooksContext();
+
   const renderedBooks = books.map((book) => {
-    return <BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit} />
+    return <BookShow key={book.id} book={book} />
   })
   return (
     <div className="book-list">
--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 import App from "./App";
 import './index.css'
+import { Provider } from "./context/books";
 
 const el = document.getElementById('root')
-
 const root = ReactDOM.createRoot(el);
 
-root.render(<App />)
+root.render(
+  <div>
+    <Provider>
+      <App />
+    </Provider>
+  </div>
+)
